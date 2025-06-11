5672f65bb68292e31c83347dea5bbdfc35939ac3
Forhad Reza <forhad.bimt@email.com>
2025-06-11 15:14:59 +0600

    added delete and all assignment api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -44,9 +44,54 @@ async function run() {
         res.status(500).send({ success: false, message: error.message });
       }
     });
+
+    app.get("/assignments", async (req, res) => {
+      const result = await assignmentCollections.find().toArray();
+      res.send(result);
+    });
+
+    
+    app.delete("/assignments/:id", async (req, res) => {
+      const id = req.params.id;
+      const email = req.query.email;
+
+      try {
+        const assignment = await assignmentCollections.findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (!assignment) {
+          return res
+            .status(404)
+            .send({ success: false, message: "Assignment not found" });
+        }
+
+        if (assignment.creator?.email !== email) {
+          return res
+            .status(403)
+            .send({
+              success: false,
+              message: "You are not authorized to delete this assignment.",
+            });
+        }
+
+        const result = await assignmentCollections.deleteOne({
+          _id: new ObjectId(id),
+        });
+
+        if (result.deletedCount > 0) {
+          res.send({ success: true });
+        } else {
+          res
+            .status(500)
+            .send({ success: false, message: "Delete failed internally." });
+        }
+      } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+      }
+    });
+
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
     app.listen(port, () => {
       console.log(`App listening on port ${port}`);
     });
